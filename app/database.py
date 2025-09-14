eb75b1cf6305a7b3c9a1dbf406594731182f8e47
annonymoys.ag <mgadia29@gmail.com>
2025-09-15 03:52:10 +0530

    addd changes

--- app/database.py
+++ app/database.py
@@ -1,7 +1,10 @@
 from flask_pymongo import PyMongo
 from flask import current_app
-
+import time
 mongo = PyMongo()
 
 def init_db(app):
+    
+    print(app.config)
+    
     mongo.init_app(app, uri=app.config["MONGO_URI"])
