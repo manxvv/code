4fdecceed78c2cc10431bbd169ff8d410ae3e3c9
annonymoys.ag <mgadia29@gmail.com>
2025-09-15 04:01:01 +0530

    addd changes

--- app/database.py
+++ app/database.py
@@ -1,10 +1,22 @@
+import pymongo, time
 from flask_pymongo import PyMongo
-from flask import current_app
-import time
+
 mongo = PyMongo()
 
 def init_db(app):
-    
-    print(app.config)
-    
-    mongo.init_app(app, uri=app.config["MONGO_URI"])
+    mongo_uri = app.config["MONGO_URI"]
+    max_retries = 10
+
+    for i in range(max_retries):
+        try:
+            client = pymongo.MongoClient(mongo_uri)
+            client.admin.command("ping")
+            print("✅ Connected to MongoDB")
+            break
+        except Exception:
+            print("Waiting for MongoDB...")
+            time.sleep(2)
+    else:
+        raise RuntimeError("Could not connect to MongoDB")
+
+    mongo.init_app(app, uri=mongo_uri)
