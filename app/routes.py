3f0dad4406e81070de12380a686b4b8743d74aa2
annonymoys.ag <mgadia29@gmail.com>
2025-09-29 00:35:47 +0530

    add new chagnes

--- app/routes.py
+++ app/routes.py
@@ -1693,7 +1693,7 @@ def create_mera_user():
         "email": "sarfraz@datayog.com",
         "hashed_password": hashed_pw,
         "ts":datetime.now().timestamp(),
-        "updated":request.user.get("sub") 
+        # "updated":request.user.get("sub") 
     }
 
     mongo.db.users.insert_one(user)
@@ -1714,6 +1714,47 @@ def check_conn():
         print("❌ Connection failed:", e)
         return "❌ Connection failed:" + str(e)
 
+
+
+@api.route("/run_gpl_audit_nokia", methods=["POST"])
+@token_required
+def run_gpl_audit_nokia_api():
+    
+    
+    
+    circle_name = request.form.get("circle")
+    
+    settings = request.files.get("settings")
+    enm_files = request.files.get("enm_file")
+    if not settings or not enm_files:
+        return jsonify({"message": "No selected settings or eFile file"}), 400
+    
+    
+    
+    if settings:
+        settings_original_filename = secure_filename(settings.filename)
+        settings_unique_filename = f"{uuid.uuid4().hex}_{settings_original_filename}"
+        settings_file_path = os.path.join(os.path.join(UPLOAD_FOLDER,"settings_nokia"), settings_unique_filename)
+        settings.save(settings_file_path)
+        
+    
+    if enm_files:
+        enm_files_original_filename = secure_filename(enm_files.filename)
+        enm_files_unique_filename = f"{uuid.uuid4().hex}_{enm_files_original_filename}"
+        enm_files_file_path = os.path.join(os.path.join(UPLOAD_FOLDER,"enm_files_nokia"), enm_files_unique_filename)
+        enm_files.save(enm_files_file_path)
+        
+        
+        
+        
+        
+    
+        
+
+    return jsonify({"message": "GPL Audit Nokia Completed successfully"}), 201
+    
+    
+    return ""
 
 
 @api.route("/run_gpl_audit", methods=["POST"])
